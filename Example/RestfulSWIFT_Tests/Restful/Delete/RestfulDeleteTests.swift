02499ee01293eb51395de2fd081f301d9077942f
Vlad Geiger <geiger.vlad.mihaly@gmail.com>
2020-04-22 14:10:01 +0200

    added empty tests

---
+++ Example/RestfulSWIFT_Tests/Restful/Delete/RestfulDeleteTests.swift
@@ -0,0 +1,27 @@
+//
+//  RestfulDeleteTests.swift
+//  RestfulSWIFT_Tests
+//
+//  Created by Vlad Geiger on 22.04.20.
+//  Copyright © 2020 Vlad Geiger. All rights reserved.
+//
+
+import XCTest
+import PromiseKit
+import RestfulSWIFT_Example
+
+class RestfulDeleteTests: XCTestCase, RestfulDelete {
+    
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+    
+    func testExample() {
+        
+    }
+    
+}
---
+++ Example/RestfulSWIFT_Tests/Tools/HTTPToolsTests.swift
@@ -0,0 +1,27 @@
+//
+//  HTTPToolsTests.swift
+//  RestfulSWIFT_Tests
+//
+//  Created by Vlad Geiger on 22.04.20.
+//  Copyright © 2020 Vlad Geiger. All rights reserved.
+//
+
+import XCTest
+import PromiseKit
+import RestfulSWIFT_Example
+
+class HTTPToolsTests: XCTestCase, HTTPTools {
+    
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+    
+    func testExample() {
+        
+    }
+    
+}
