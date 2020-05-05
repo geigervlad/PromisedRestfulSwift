55f2d68031c0100aeec9868cc6130ab68e5e203d
ManDon5 <man-don@t-online.de>
2020-05-05 11:12:23 +0200

    - Code coverage on scheme - Write tests - Some minor changes on the read tests

--- Example/RestfulSWIFT_Tests/Extensions/EncodableTests.swift
+++ Example/RestfulSWIFT_Tests/Extensions/EncodableTests.swift
@@ -10,16 +10,10 @@ import XCTest
 import PromiseKit
 import RestfulSWIFT_Example
 
-struct TestStructureEncodable: Encodable {
-    var string: String
-    var int: Int
-    var bool: Bool
-}
-
 class EncodableTests: XCTestCase {
-            
+    
     func testJsonTransformationSuccess() {
-        let structure = TestStructureEncodable(string: "I am a test data", int: 1512, bool: true )
+        let structure = TestStructureCodable(string: "I am a test data", int: 1512, bool: true )
         let expectedString = "{\"string\":\"I am a test data\",\"int\":1512,\"bool\":true}"
         let expectation = XCTestExpectation()
         structure.toJsonData().done { actualData in
@@ -32,18 +26,18 @@ class EncodableTests: XCTestCase {
         wait(for: [expectation], timeout: 1)
     }
     
-      func testJsonTransformationFailure() {
-          let structure = TestStructureEncodable(string: "I am a test data", int: 1512, bool: true )
-          let expectedString = "{\"string\":\"I am a  test data\",\"int\":1512,\"bool\":true}"
-          let expectation = XCTestExpectation()
-          structure.toJsonData().done { actualData in
-              let actualString = String(data: actualData, encoding: .utf8)
-              XCTAssertNotEqual(actualString, expectedString)
-              expectation.fulfill()
-          }.catch { error in
-              XCTFail("ERROR: Unexpected error: \(error) occured")
-          }
-          wait(for: [expectation], timeout: 1)
-      }
+    func testJsonTransformationFailure() {
+        let structure = TestStructureCodable(string: "I am a test data", int: 1512, bool: true )
+        let expectedString = "{\"string\":\"I am a  test data\",\"int\":1512,\"bool\":true}"
+        let expectation = XCTestExpectation()
+        structure.toJsonData().done { actualData in
+            let actualString = String(data: actualData, encoding: .utf8)
+            XCTAssertNotEqual(actualString, expectedString)
+            expectation.fulfill()
+        }.catch { error in
+            XCTFail("ERROR: Unexpected error: \(error) occured")
+        }
+        wait(for: [expectation], timeout: 1)
+    }
     
 }
