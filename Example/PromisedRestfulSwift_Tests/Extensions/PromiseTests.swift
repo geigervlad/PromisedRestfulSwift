375c769a6ee74d927d4aa2bb92bf8bff89580982
Vlad Geiger <geiger.vlad.mihaly@gmail.com>
2020-05-08 08:20:11 +0200

    changed name from RestfulSWIFT to PromisedRestfulSwift

---
+++ Example/PromisedRestfulSwift_Tests/Extensions/PromiseTests.swift
@@ -0,0 +1,57 @@
+//
+//  PromiseTests.swift
+//  PromisedRestfulSwift_Tests
+//
+//  Created by Vlad Geiger on 22.04.20.
+//  Copyright © 2020 Vlad Geiger. All rights reserved.
+//
+
+import XCTest
+import PromiseKit
+import PromisedRestfulSwift_Example
+
+class PromiseTests: XCTestCase {
+    
+    var promise: Promise<Void>!
+    var resolver: Resolver<Void>!
+    
+    override func setUp() {
+        promise = Promise { resolver in
+            self.resolver = resolver
+        }
+    }
+    
+    override func tearDown() {
+        resolver = nil
+        promise = nil
+    }
+    
+    func testPromiseDoesNotTimeout() {
+        let expectation = XCTestExpectation()
+        DispatchQueue(label: "testTimeoutSuccess").asyncAfter(deadline: .now() + 1) {
+            self.resolver.fulfill(())
+        }
+        promise.timeout(after: 2).done {
+            expectation.fulfill()
+        }.catch { error in
+            XCTFail("ERROR: Unexpected error: \(error) occured")
+        }
+        wait(for: [expectation], timeout: 3)
+    }
+    
+    func testPromiseTimeout() {
+        let expectation = XCTestExpectation()
+        promise.timeout(after: 2).done {
+            XCTFail("ERROR: Promise should not fullfil")
+        }.catch { error in
+            switch error {
+            case PromiseErrors.timeout:
+                expectation.fulfill()
+            default:
+                XCTFail("ERROR: Unexpected error: \(error) occured")
+            }
+        }
+        wait(for: [expectation], timeout: 3)
+    }
+        
+}
---
+++ Example/PromisedRestfulSwift_Tests/Restful/Read/RestfulReadTests.swift
@@ -0,0 +1,154 @@
+//
+//  RestfulReadTests.swift
+//  PromisedRestfulSwift_Tests
+//
+//  Created by Vlad Geiger on 22.04.20.
+//  Copyright © 2020 Vlad Geiger. All rights reserved.
+//
+
+import XCTest
+import PromiseKit
+import PromisedRestfulSwift_Example
+
+class RestfulReadTests: XCTestCase, RestfulRead {
+    
+    var exampleUri: URL!
+    var response: HTTPResponseType!
+    
+    override func setUp() {
+        exampleUri = URL(string: "https://testExample.test")!
+        response = nil
+    }
+    
+    override func tearDown() {
+        exampleUri = nil
+        response = nil
+    }
+    
+    func execute(_ request: URLRequest) -> Promise<HTTPResponseType> {
+        return Promise { resolver in
+            resolver.fulfill(response)
+        }
+    }
+    
+    // MARK: - Test for "func read<U: Decodable>(_ url: URL) -> Promise<U>"
+    
+    func testReadFailureFatal() {
+        response = (nil, nil, GeneralErrors.fatal)
+        let promise: Promise<[String]> = read(exampleUri)
+        let expectation = XCTestExpectation()
+        promise.done { result in
+            XCTFail("ERROR: Unexpected result: \(result)")
+        }.catch { error in
+            switch error {
+            case GeneralErrors.fatal:
+                expectation.fulfill()
+            default:
+                XCTFail("ERROR: Unexpected error: \(error) occured")
+            }
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    
+    func testReadFailureFailedToTransformToHTTPURLResponse() {
+        response = (nil, nil, nil)
+        let promise: Promise<[String]> = read(exampleUri)
+        let expectation = XCTestExpectation()
+        promise.done { result in
+            XCTFail("ERROR: Unexpected result: \(result)")
+        }.catch { error in
+            switch error {
+            case DecodingErrors.failedToTransformToHTTPURLResponse:
+                expectation.fulfill()
+            default:
+                XCTFail("ERROR: Unexpected error: \(error) occured")
+            }
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    
+    func testReadFailureInvalidHttpCode() {
+        let httpUrlResponse = HTTPURLResponse(url: exampleUri,
+                                              statusCode: 400,
+                                              httpVersion: nil,
+                                              headerFields: nil)
+        response = (nil, httpUrlResponse, nil)
+        let promise: Promise<[String]> = read(exampleUri)
+        let expectation = XCTestExpectation()
+        promise.done { result in
+            XCTFail("ERROR: Unexpected result: \(result)")
+        }.catch { error in
+            switch error {
+            case ValidationErrors.invalidHttpCode(code: httpUrlResponse?.statusCode):
+                expectation.fulfill()
+            default:
+                XCTFail("ERROR: Unexpected error: \(error) occured")
+            }
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    
+    func testReadFailureFailedToExtractData() {
+        let httpUrlResponse = HTTPURLResponse(url: exampleUri,
+                                              statusCode: 200,
+                                              httpVersion: nil,
+                                              headerFields: nil)
+        response = (nil, httpUrlResponse, nil)
+        let promise: Promise<[String]> = read(exampleUri)
+        let expectation = XCTestExpectation()
+        promise.done { result in
+            XCTFail("ERROR: Unexpected result: \(result)")
+        }.catch { error in
+            switch error {
+            case DecodingErrors.failedToExtractData:
+                expectation.fulfill()
+            default:
+                XCTFail("ERROR: Unexpected error: \(error) occured")
+            }
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    
+    func testReadFailureDecodingErrorDataCorrupted() {
+        let httpUrlResponse = HTTPURLResponse(url: exampleUri,
+                                              statusCode: 200,
+                                              httpVersion: nil,
+                                              headerFields: nil)
+        response = (Data(), httpUrlResponse, nil)
+        let promise: Promise<[String]> = read(exampleUri)
+        let expectation = XCTestExpectation()
+        promise.done { result in
+            XCTFail("ERROR: Unexpected result: \(result)")
+        }.catch { error in
+            switch error {
+            case is DecodingError:
+                expectation.fulfill()
+            default:
+                XCTFail("ERROR: Unexpected error: \(error) occured")
+            }
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    
+    func testReadSuccess() {
+        let httpUrlResponse = HTTPURLResponse(url: exampleUri,
+                                              statusCode: 200,
+                                              httpVersion: nil,
+                                              headerFields: nil)
+        let structure = TestStructureCodable(string: "I am a test data", int: 1512, bool: true )
+        let data = try! JSONEncoder().encode(structure)
+        response = (data, httpUrlResponse, nil)
+        let promise: Promise<TestStructureCodable> = read(exampleUri)
+        let expectation = XCTestExpectation()
+        promise.done { result in
+            XCTAssertEqual(result.string, structure.string)
+            XCTAssertEqual(result.int, structure.int)
+            XCTAssertEqual(result.bool, structure.bool)
+            expectation.fulfill()
+        }.catch { error in
+            XCTFail("ERROR: Unexpected error: \(error) occured")
+        }
+        wait(for: [expectation], timeout: 2)
+    }
+    
+}
