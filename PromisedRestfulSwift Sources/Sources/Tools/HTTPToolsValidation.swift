c68ded41f43c7a09fab3f8ddf2b89cefb7875556
Vlad Geiger <geiger.vlad.mihaly@gmail.com>
2020-05-11 09:30:36 +0200

    removed overwrite possibility for http tools validation: custom implementation can be implemented within the custom service

--- PromisedRestfulSwift Sources/Sources/Tools/HTTPToolsValidation.swift
+++ PromisedRestfulSwift Sources/Sources/Tools/HTTPToolsValidation.swift
@@ -15,28 +15,16 @@ public protocol HTTPToolsValidation: HTTPTools {
     /// Defines the Type of the Error Object in case of a server error, for example: validation
     associatedtype ErrorObjectType: Decodable
     
-    /// Checks a HTTPResponse for StatusCode, Error, Data and tries to transforms the data to expected Decodable Structure
-    /// In case of HTTP Status Codes Bad Request(400) or Conflict(409) the server response gets decoded with the defined error object type
-    /// - Parameter response: the HTTPResponse to check
-    /// - Returns: The Decoded Entity
-    func toValidatedEntityWithError<T: Decodable>(_ response: HTTPResponseType) throws -> T
-    
-    /// Checks a HTTPResponse for StatusCode, Error
-    /// In case of HTTP Status Codes Bad Request(400) or Conflict(409) the server response gets decoded with the defined error object type
-    /// - Parameter response: the HTTPResponse to check
-    func toValidatedError(_ response: HTTPResponseType) throws
-    
-    /// Checks a HTTPResponse for StatusCode, Error, and tries to extract the value of the HTTP Location Header
-    /// /// In case of HTTP Status Codes Bad Request(400) or Conflict(409) the server response gets decoded with the defined error object type
-    /// - Parameter response: the HTTPResponse to check
-    /// - Returns: The Value of the Location Header
-    func toValidatedLocationWithError(_ response: HTTPResponseType) throws -> String
 }
 
 // MARK: Default Implementation
 
 public extension HTTPToolsValidation {
     
+    /// Checks a HTTPResponse for StatusCode, Error, Data and tries to transforms the data to expected Decodable Structure
+    /// In case of HTTP Status Codes Bad Request(400) or Conflict(409) the server response gets decoded with the defined error object type
+    /// - Parameter response: the HTTPResponse to check
+    /// - Returns: The Decoded Entity
     func toValidatedEntityWithError<T: Decodable>(_ response: HTTPResponseType) throws -> T {
         do {
             return try toValidatedEntity(response)
@@ -45,6 +33,9 @@ public extension HTTPToolsValidation {
         }
     }
     
+    /// Checks a HTTPResponse for StatusCode, Error
+    /// In case of HTTP Status Codes Bad Request(400) or Conflict(409) the server response gets decoded with the defined error object type
+    /// - Parameter response: the HTTPResponse to check
     func toValidatedError(_ response: HTTPResponseType) throws {
         do {
             _ = try toStatusCodeValidated(toErrorValidated(response))
@@ -53,6 +44,10 @@ public extension HTTPToolsValidation {
         }
     }
     
+    /// Checks a HTTPResponse for StatusCode, Error, and tries to extract the value of the HTTP Location Header
+    /// In case of HTTP Status Codes Bad Request(400) or Conflict(409) the server response gets decoded with the defined error object type
+    /// - Parameter response: the HTTPResponse to check
+    /// - Returns: The Value of the Location Header
     func toValidatedLocationWithError(_ response: HTTPResponseType) throws -> String {
         do {
             return try toValidatedLocation(response)
@@ -61,7 +56,7 @@ public extension HTTPToolsValidation {
         }
     }
     
-    func extractServerError(_ error: Error, _ response: HTTPResponseType) throws -> Error {
+    private func extractServerError(_ error: Error, _ response: HTTPResponseType) throws -> Error {
         switch error {
         case ValidationErrors.invalidHttpCode(let httpCode):
             if httpCode == HTTPStatusCodes.badRequest.rawValue || httpCode == HTTPStatusCodes.conflict.rawValue {
